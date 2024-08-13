46e048a5de3d18b9e897ea0262df9a58111123ab
Rusty <105621220+balconsoftakhmedov@users.noreply.github.com>
2024-08-13 18:25:38 +0500

    fixes

--- vc_templates/stm_wcmap_customer_feedback.php
+++ vc_templates/stm_wcmap_customer_feedback.php
@@ -14,59 +14,67 @@ extract( $atts );
 			>
 		</div>
 		<div class="item-box">
-			<div class="text-8f">
-				<div class="stars">
-					<div class="vector-90">
-						<div class="vector-91"></div>
-						<div class="vector-92"></div>
-						<div class="vector-93"></div>
-						<div class="vector-94"></div>
-						<div class="description"></div>
-					</div>
-					<span class="quote-close"
-					>Crux interfacing gives all the best devices. It is the most
+			<div class="row">
+				<div class="col-xs-12 col-sm-6 col-md-4">
+					<div class="text-8f">
+						<div class="stars">
+							<div class="vector-90">
+								<div class="vector-91"></div>
+								<div class="vector-92"></div>
+								<div class="vector-93"></div>
+								<div class="vector-94"></div>
+								<div class="description"></div>
+							</div>
+							<span class="quote-close-9e"
+							>Crux interfacing gives all the best devices. It is the most
                   versatile I could get my hands on. Compared to all the others
                   I have tried...</span
-					>
-				</div>
-				<div class="customer-name"></div>
-				<span class="item-box-95">Andrew Jackson</span>
-			</div>
-			<div class="text-96">
-				<div class="stars-97">
-					<div class="vector-98">
-						<div class="vector-99"></div>
-						<div class="vector-9a"></div>
-						<div class="vector-9b"></div>
-						<div class="vector-9c"></div>
-						<div class="description-9d"></div>
+							>
+						</div>
+						<div class="customer-name"></div>
+						<span class="item-box-95">Andrew Jackson</span>
 					</div>
-					<span class="quote-close-9e"
-					>I prefer crux interfacing solution. I could get my hands on.
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-4">
+					<div class="text-8f">
+						<div class="stars-97">
+							<div class="vector-98">
+								<div class="vector-99"></div>
+								<div class="vector-9a"></div>
+								<div class="vector-9b"></div>
+								<div class="vector-9c"></div>
+								<div class="description-9d"></div>
+							</div>
+							<span class="quote-close-9e"
+							>I prefer crux interfacing solution. I could get my hands on.
                   Compared to all the others I have tried, this is the best
                   premium devices out there! Thanks</span
-					>
-				</div>
-				<div class="customer-name-9f"></div>
-				<span class="text-35">John Mike</span>
-			</div>
-			<div class="item-box-a0">
-				<div class="text-a1">
-					<div class="stars-a2">
-						<div class="vector-a3"></div>
-						<div class="vector-a4"></div>
-						<div class="vector-a5"></div>
-						<div class="vector-a6"></div>
-						<div class="vector-a7"></div>
+							>
+						</div>
+						<div class="customer-name"></div>
+						<span class="item-box-95">John Mike</span>
 					</div>
-					<span class="description-a8"
-					>Compared to all the others I have tried, this is the best
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-4">
+					<div class="text-8f">
+						<div class="text-a1">
+							<div class="stars-a2">
+								<div class="vector-a3"></div>
+								<div class="vector-a4"></div>
+								<div class="vector-a5"></div>
+								<div class="vector-a6"></div>
+								<div class="vector-a7"></div>
+							</div>
+							<span class="quote-close-9e"
+							>Compared to all the others I have tried, this is the best
                   premium car devices out there! I love this website,
                   Amazing</span
-					>
+							>
+						</div>
+						<div class="customer-name"></div>
+						<span class="item-box-95">Alex Jackson</span>
+					</div>
 				</div>
-				<div class="quote-close-a9"></div>
-				<span class="author-name">Alex Jackson</span>
 			</div>
 		</div>
 	</div>
