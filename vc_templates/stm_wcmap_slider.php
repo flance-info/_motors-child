e7408d516148ca17b943e3007293e295ab1028d6
Rusty <105621220+balconsoftakhmedov@users.noreply.github.com>
2024-08-14 09:19:24 +0500

    video slider

--- vc_templates/stm_wcmap_slider.php
+++ vc_templates/stm_wcmap_slider.php
@@ -48,6 +48,59 @@ if ( ! empty( $videos ) ) : ?>
 	</div>
 <?php endif; ?>
 
+<div class="section-video container-fluid">
+    <div class="row section-video-span">
+        <!-- Previous Button -->
+        <div class="col-xs-12 col-sm-1 text-center stm-button-arrow">
+            <div class="button-74">
+                <div class="icon-75">
+                    <div class="elements-76">
+                        <div class="vector-77"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Video Slider Area -->
+        <div class="col-xs-12 col-sm-10 ">
+            <!-- First Video Frame -->
+            <div class="frame-6c">
+                <div class="video embed-responsive ">
+                    <iframe allowfullscreen data-src="https://www.youtube.com/embed/BGU58eoAOaw" class="embed-responsive-item lazyloaded" src="https://www.youtube.com/embed/BGU58eoAOaw" loading="lazy"></iframe>
+                </div>
+                <div class="info-6f">
+                    <span class="video-heading">Video: How to install into the car our product</span>
+                    <span class="video-text">Video instruction, demo text You’ll be able to effortlessly build cars listings, set taxonomies, make use of the optimized search filter.</span>
+                </div>
+            </div>
+
+            <!-- Second Video Frame -->
+            <div class="frame-6c" style="display: none;">
+                <div class="video embed-responsive embed-responsive-16by9">
+                    <iframe allowfullscreen data-src="https://www.youtube.com/embed/2Z4m4lnjxkY" class="embed-responsive-item lazyloaded" src="https://www.youtube.com/embed/2Z4m4lnjxkY" loading="lazy"></iframe>
+                </div>
+                <div class="info-6f">
+                    <span class="video-heading">Video: Another Product Installation</span>
+                    <span class="video-text">Text about the video</span>
+                </div>
+            </div>
+        </div>
+
+        <!-- Next Button -->
+        <div class="col-xs-12 col-sm-1 text-center stm-button-arrow">
+            <div class="button-70">
+                <div class="icon-71">
+                    <div class="elements-72">
+                        <div class="vector-73"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
 <script>
 jQuery(document).ready(function($) {
     var currentIndex = 0;
