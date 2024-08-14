7dc2c6bd22fed3f0e4eabe511505f1c62c3fd7c3
Rusty <105621220+balconsoftakhmedov@users.noreply.github.com>
2024-08-14 11:29:08 +0500

    menu ready

--- partials/header/header-custom.php
+++ partials/header/header-custom.php
@@ -4,14 +4,7 @@ $menu_name  = 'primary'; // Replace with your menu slug or ID
 $locations  = get_nav_menu_locations(); // Get all menu locations
 $menu_id    = $locations[ $menu_name ]; // Get the menu ID based on the location
 $menu_items = wp_get_nav_menu_items( $menu_id ); // Retrieve the menu items
-wp_nav_menu( array(
-    'theme_location' => 'primary',
-    'container'      => false,
-    'items_wrap'     => '<div class="menu-container">%3$s</div>', // Wrap items in a div container
-    'link_before'    => '<span class="link-common">',
-    'link_after'     => '</span>',
-    'walker'         => new Custom_Walker_Nav_Menu(), // Custom walker to handle submenus
-) );
+
 
 ?>
 <div class="section-intro">
@@ -23,35 +16,20 @@ wp_nav_menu( array(
 			<div class="column">
 				<div class="column-1">
 					<?php if ( $menu_items ): ?>
-						<ul class="menu">
+
 							<?php foreach ( $menu_items as $item ): ?>
-								<?php if ( $item->menu_item_parent == 0 ): // It's a top-level item ?>
-									<li class="menu-item">
-                        <span class="link">
-                            <a href="<?php echo esc_url( $item->url ); ?>">
-                                <?php echo esc_html( $item->post_title ); ?>
-                            </a>
-                        </span>
-										<?php
-										// Check if this item has children
-										$submenu_items = array_filter( $menu_items, function ( $submenu_item ) use ( $item ) {
-											return $submenu_item->menu_item_parent == $item->ID;
-										} );
-										if ( ! empty( $submenu_items ) ): ?>
-											<ul class="submenu">
-												<?php foreach ( $submenu_items as $submenu_item ): ?>
-													<li class="submenu-item">
-														<a href="<?php echo esc_url( $submenu_item->url ); ?>">
-															<?php echo esc_html( $submenu_item->post_title ); ?>
-														</a>
-													</li>
-												<?php endforeach; ?>
-											</ul>
-										<?php endif; ?>
-									</li>
+								<?php if ( $item->menu_item_parent == 0 && $item->post_title !='dgwt_wcas_search_box'): // It's a top-level item ?>
+
+										<span class="link">
+											<a href="<?php echo esc_url( $item->url ); ?>">
+												<?php echo esc_html( $item->post_title ); ?>
+											</a>
+										</span>
+
+
 								<?php endif; ?>
 							<?php endforeach; ?>
-						</ul>
+
 					<?php endif; ?>
 				</div>
 
